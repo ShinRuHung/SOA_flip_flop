748b4d896d7d95cba5b54e31f63f451f8a04f2f8
108065702 <cin810619121dy@gmail.com>
2021-10-06 15:59:40 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,19 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    rows = tsv.split("\n").map { |line| line.split("\t") }
+    tsv_headers = rows.first
+    @data = rows[1..].map { |row| tsv_headers.zip(row).to_h }
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    @data << ''
+    @data.reduce { |n1, n2| "#{n1}\n#{n2}" }
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,31 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  def retrieve_key(input_hash)
+    arr = input_hash.map { |k, _v| k }
+    arr.reduce { |n1, n2| "#{n1}\t#{n2}" }
+  end
+
+  def retrieve_value(input_hash)
+    arr = input_hash.map { |_k, v| v }
+    arr.reduce { |n1, n2| "#{n1}\t#{n2}" }
+  end
+
+  # take_tsv: converts a String with TSV data into @data
+  # parameter: tsv - a String in TSV format
+  def take_yaml(yaml)
+    rows = YAML.safe_load(yaml)
+    @data = rows.map { |data| retrieve_value(data) }
+    @data.insert(0, retrieve_key(rows[0]))
+    @data.reduce { |n1, n2| "#{n1}\n#{n2}" }
+  end
+
+  # to_tsv: converts @data into tsv string
+  # returns: String in TSV format
+  def to_yaml
+    @data.to_yaml
+  end
+end
